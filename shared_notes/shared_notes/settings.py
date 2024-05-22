dcc735010e9f58f43449654a45041e6f9a05a5f6
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-05-22 18:13:15 +0200

    add DNS for database

--- shared_notes/shared_notes/settings.py
+++ shared_notes/shared_notes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api"  # api for PostgreSQL
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'shared_notes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "CloudComputingDatabase",
+        "USER": "Admin",
+        "PASSWORD": "TestPa$$word123",
+        "HOST": "db.djangopostgresql.com",
+        "PORT": "5432",
     }
 }
 
--- terraform/database.tf
+++ terraform/database.tf
@@ -30,3 +30,21 @@ resource "google_sql_user" "admin" {
   password = "TestPa$$word123"
   project  = google_project.this.project_id
 }
+
+resource "google_dns_managed_zone" "this" {
+  project  = google_project.this.project_id
+  name     = "django-postgresql-zone"
+  dns_name = "djangopostgresql.com."
+
+  depends_on = [google_project_service.dns]
+}
+
+resource "google_dns_record_set" "this" {
+  project      = google_project.this.project_id
+  name         = "db.djangopostgresql.com."
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.this.name
+
+  rrdatas = [google_sql_database_instance.this.public_ip_address]
+}
