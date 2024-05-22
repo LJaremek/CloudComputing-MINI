4dd8eba2d7a8b4988f4eba59c901f0e27468c8f4
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-05-22 18:20:32 +0200

    create django sql models

---
+++ shared_notes/api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    pass_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    shared_at = models.DateTimeField(auto_now=True)
+
+
+class SharedNote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    shared_at = models.DateTimeField(auto_now=True)
+    permission_type = models.CharField(max_length=50)
