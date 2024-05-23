942ed05013af278e3894209952206a20faca048a
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-05-23 20:52:51 +0200

    fix columns

--- shared_notes/api/models.py
+++ shared_notes/api/models.py
@@ -6,7 +6,6 @@ class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     pass_hash = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "users"
@@ -18,7 +17,7 @@ class User(models.Model):
 
 class Note(models.Model):
     id_note = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, models.CASCADE, db_column="id_user")
+    id_user = models.ForeignKey(User, models.CASCADE, db_column="id_user")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     shared_at = models.DateTimeField(auto_now=True)
@@ -26,20 +25,20 @@ class Note(models.Model):
     class Meta:
         db_table = "notes"
         indexes = [
-            models.Index(fields=["user"]),
+            models.Index(fields=["id_user"]),
         ]
 
 
 class SharedNote(models.Model):
     id_shared = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, models.CASCADE, db_column="id_user")
-    note = models.ForeignKey(Note, models.CASCADE, db_column="id_note")
+    id_user = models.ForeignKey(User, models.CASCADE, db_column="id_user")
+    id_note = models.ForeignKey(Note, models.CASCADE, db_column="id_note")
     shared_at = models.DateTimeField(auto_now=True)
     permission_type = models.CharField(max_length=50)
 
     class Meta:
         db_table = "shared_notes"
         indexes = [
-            models.Index(fields=["user"]),
-            models.Index(fields=["note"]),
+            models.Index(fields=["id_user"]),
+            models.Index(fields=["id_note"]),
         ]
