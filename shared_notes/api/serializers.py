ee89eb0cdaacbd9f1fd4bc4bd0984e7fbb5894fd
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-05-23 21:09:50 +0200

    fix serializer

--- shared_notes/api/serializers.py
+++ shared_notes/api/serializers.py
@@ -6,16 +6,18 @@ from .models import User, Note, SharedNote
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'pass_hash']
+        fields = ["id_user", "username", "email", "pass_hash"]
 
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ['id', 'user', 'content', 'created_at', 'shared_at']
+        fields = ["id_note", "id_user", "content", "created_at", "shared_at"]
 
 
 class SharedNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = SharedNote
-        fields = ['id', 'user', 'note', 'shared_at', 'permission_type']
+        fields = [
+            "id_shared", "id_user", "id_note", "shared_at", "permission_type"
+            ]
