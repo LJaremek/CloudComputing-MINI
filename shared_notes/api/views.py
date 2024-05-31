7a9f4eb2a7f6c360cfbe6c9bfd367fb91d01c3ec
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-05-31 15:16:48 +0200

    add new endpoints

--- shared_notes/api/views.py
+++ shared_notes/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import status
 
 from .serializers import UserSerializer, NoteSerializer, SharedNoteSerializer
+from .serializers import SharedNoteDetailSerializer
 from .models import User, Note, SharedNote
 
 
@@ -62,3 +63,28 @@ class DeleteSharedNoteView(generics.DestroyAPIView):
         shared_note = self.get_object()
         self.perform_destroy(shared_note)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SharedNotesListView(generics.ListAPIView):
+    serializer_class = SharedNoteDetailSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        return SharedNote.objects.filter(id_user=user_id)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class NoteDetailView(generics.RetrieveAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    lookup_field = "id_note"
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "id_user"
