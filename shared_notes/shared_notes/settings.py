482ff2250567551d0e30e2846494648c56c091bc
PawelGlr <pawelek979@gmail.com>
2024-06-09 01:53:13 +0200

    revert db to postgreslq

--- shared_notes/shared_notes/settings.py
+++ shared_notes/shared_notes/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'shared_notes.wsgi.application'
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "CloudComputingDatabase",
+        "USER": "Admin",
+        "PASSWORD": "TestPa$$word123",
+        "HOST": "db.djangopostgresql.com",
+        "PORT": "5432",
     }
 }
 
